c54b461898f4db3b680f495390843170338cb666
Raghavan789 <mavkrishnaraghavan@gmail.com>
2024-06-18 12:36:37 +0530

    Practicing

--- index.js
+++ index.js
@@ -15,15 +15,32 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
+// Insert one document
 const user1 = new User({
-  name: "adam",
+  name: "adasfsfvm",
   email: "adam@gmail.com",
   age: 45
 });
 
-user1.save()
-  .then(() => console.log('User saved'))
-  .catch(err => console.log(err));
+// // Insert many documents
+// const users = [
+//   { name: "Alice raj", email: "alice@example.com", age: 25 },
+//   { name: "Bob kumar", email: "bob@example.com", age: 28 },
+//   { name: "Charlie Sai", email: "charlie@example.com", age: 32 },
+//   { name: "David Bhasha", email: "david@example.com", age: 30 },
+//   { name: "Eve Man", email: "eve@example.com", age: 27 }
+// ];
 
+// User.insertMany(users)
+//   .then(()=>{
+//     console.log('users saved');
+//   })
+//   .catch(err=>console.log(err));
 
+  User.deleteMany({age:{$lt:28}})
+  .then(deletedResult =>{
+    console.log('Delted${deletedResult.deleteCount} users ');
 
+  })
+
+  .catch(err=>console.log(err));
